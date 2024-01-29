772b4ee071744c9d5dd7e744996b923667d1539e
6lueparr0t <6lueparr0t@gmail.com>
2024-01-30 02:01:09 +0900

    feat : done 04-template

--- 04-template/app.js
+++ 04-template/app.js
@@ -1,17 +1,20 @@
 // const http = require("http");
 
 const express = require("express");
-// const favicon = require("serve-favicon");
+const favicon = require("serve-favicon");
 const path = require("path");
 const rootDir = require("./util/path");
 
 const app = express();
 const shopRoutes = require("./routes/shop");
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(favicon(path.join(rootDir, "public", "favicon.ico")));
+app.use(favicon(path.join(rootDir, "public", "favicon.ico")));
 app.use(express.static(path.join(rootDir, 'public')));
 
 app.use("/", (req, res, next) => {
@@ -19,11 +22,12 @@ app.use("/", (req, res, next) => {
   next();
 });
 
-app.use("/admin", adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "404.html"));
+  // res.sendFile(path.join(rootDir, "views", "404.html"));
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
--- 04-template/routes/shop.js
+++ 04-template/routes/shop.js
@@ -1,11 +1,23 @@
-const path = require("path");
+// const path = require("path");
+// const rootDir = require("../util/path");
 const express = require("express");
-const rootDir = require("../util/path");
 const router = express.Router();
+
+const adminData = require('./admin');
 
 router.get("/", (req, res, next) => {
   // res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  // res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+  console.log(products);
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
